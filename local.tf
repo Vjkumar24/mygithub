f5498b8b7a6fe347319348c86d7c57e1cda5c0e8
root <root@ip-172-31-41-23.ec2.internal>
2023-08-29 07:57:51 +0000

    local.tf

---
+++ local.tf
@@ -0,0 +1,14 @@
+resource "local_file" "devops" {
+filename = "/root/twsubham/vj.txt"
+content = "And also I want to become a AWS Engineer who know handson services"
+}
+
+resource "random_string" "rand_str" {
+length = 16
+special = true
+override_special = "7477Ejjk**&&###$$$"
+}
+
+output "random-string" {
+value = random_string.rand_str[*].result
+}
