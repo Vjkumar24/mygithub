2e126eb7d2f4a449815f4dc09c60716cd80279c1
root <root@ip-172-31-44-104.ec2.internal>
2023-08-29 19:01:12 +0000

    map

--- variable.tf
+++ variable.tf
@@ -1,1 +1,11 @@
-variable "export_concept" {}
+variable "filename" {
+default = "/root/variable_concept/mygithub/map1.txt"
+}
+
+variable "content_map" {
+type = map
+default = {
+"content1" = "This is Vjkumar"
+"content2" = "I belong to Jabalpur"
+}
+}
